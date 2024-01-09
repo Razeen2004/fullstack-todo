989b02235c544e95201c30cd78fa1565fbafbf3c
RazeenBhai <razeenbaig10@gmail.com>
2024-01-09 22:28:21 +0500

    cahnges

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,30 +4,24 @@ import CreateTodo from './components/CreateTodo';
 import { useState, useEffect } from 'react';
 import { useRef } from "react";
 import { motion } from "framer-motion";
-
 function App() {
 
   let [todo, setTodo] = useState([]);
   const constraintsRef = useRef(null);
-  
-  useEffect(() => {
-    const fetchTodos = async () => {
-      try {
-        let data = await fetch("http://localhost:8000/todos");
-        let todos = await data.json();
-        setTodo(todos);
-      } catch (error) {
-        console.error("Error fetching todos:", error);
-      }
-    };
 
-    fetchTodos();
+  useEffect(() => {
+    fetch("http://localhost:8000/todos")
+      .then((response) => {
+        response.json((finaldata) => {
+          setTodo(finaldata)
+        })
+      })
   }, [todo]);
 
   return (
     <motion.div ref={constraintsRef} className='body'>
-        <CreateTodo constraintsRef={constraintsRef} />
-        <Todo constraintsRef={constraintsRef} todos={todo} />
+      <CreateTodo constraintsRef={constraintsRef} />
+      <Todo constraintsRef={constraintsRef} todos={todo} />
     </motion.div>
   );
 }
